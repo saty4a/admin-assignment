a75692b71576aaf12d92ce45e703093095ac4047
satyabrata biswal <86743992+saty4a@users.noreply.github.com>
2024-02-23 12:48:23 +0530

    Update urls.jsx

--- src/config/urls.jsx
+++ src/config/urls.jsx
@@ -1,4 +1,4 @@
-const domainName = "http://localhost:3000/api";
+const domainName = "https://dashboard-backend-psi.vercel.app/api";
 
 export const urls = {
     uploadImage: `${domainName}/upload-images`,
